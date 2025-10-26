9a6872af4e6d3edd2e843dc78940732506d08d2d
Diana Castaño <dianaczapata@gmail.com>
2025-10-26 10:47:54 -0500

    modificar los estilos de App y main e incluir la fuente Arimo

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import AppRoutes from "./routes/AppRoutes";
+import "./styles/global-styles.css";
+import "@fontsource/arimo";
+
+function App() {
+  return (
+    <AppRoutes />
+  );
+}
+
+export default App;
