8c7a9b30e8b35026653d0a2224861203c65ef572
Diana Castaño <dianaczapata@gmail.com>
2025-11-29 02:26:53 -0500

    agregar clase compiladora

---
+++ Backend/src/main/java/com/odontoapp/OdontoAppApplication.java
@@ -0,0 +1,15 @@
+package com.odontoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontoAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OdontoAppApplication.class, args);
+		System.out.println("System on!!");
+	}
+
+}
