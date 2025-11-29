5676dfbada662f59681757b7b276dfbb5892d844
Diana Castaño <dianaczapata@gmail.com>
2025-11-29 14:29:07 -0500

    eliminar typo

--- Frontend/src/pages/UserPanel.jsx
+++ Frontend/src/pages/UserPanel.jsx
@@ -18,7 +18,7 @@ const UserPanel = () => {
     <div className="p-6 bg-gray-50 min-h-screen text-gray-900">
       {/* Header */}
       <div className="mb-8">
-        <h1 className="text-2xl font-bold">Bienvenida, Diana</h1>
+        <h1 className="text-2xl font-bold">Bienvenido</h1>
         <p className="text-gray-600">Lunes, 16 de junio de 2025</p>
       </div>
 
