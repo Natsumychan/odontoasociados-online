9352fec721cdb1a85c5d2577c0da6f3781289cef
Diana Castaño <dianaczapata@gmail.com>
2025-11-29 02:32:11 -0500

    clases para el control de login

---
+++ Backend/src/main/java/com/odontoapp/dto/auth/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.odontoapp.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String role;
+}
+
