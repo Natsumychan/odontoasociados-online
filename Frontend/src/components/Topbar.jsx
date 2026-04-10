6d884a71bce422e2ad9e9722133eb809178e6f95
Diana Castaño <dianaczapata@gmail.com>
2026-04-10 10:24:38 -0500

    update profile's name font size

--- Frontend/src/components/Topbar.jsx
+++ Frontend/src/components/Topbar.jsx
@@ -1,30 +1,29 @@
 import { useAuth } from "../context/AuthContext";
 
 const Topbar = ({ onMenuClick }) => {
-  const { user } = useAuth();
+	const { user } = useAuth();
 
-  return (
-    <header className="fixed top-0 left-0 w-full bg-[#00439C] text-white flex justify-between items-center px-4 py-3 shadow-md z-50">
-      {/* 🔹 Botón hamburguesa (solo móvil) */}
-      <button
-        onClick={onMenuClick}
-        className="text-white text-2xl mr-2 md:hidden"
-      >
-        ☰
-      </button>
+	return (
+		<header className='fixed top-0 left-0 w-full bg-[#00439C] text-white flex justify-between items-center px-4 py-3 shadow-md z-50'>
+			{/* 🔹 Botón hamburguesa (solo móvil) */}
+			<button
+				onClick={onMenuClick}
+				className='text-white text-2xl mr-2 md:hidden'>
+				☰
+			</button>
 
-      <h1 className="text-lg font-semibold flex-1">Odontólogos Asociados</h1>
+			<h1 className='text-lg font-semibold flex-1'>Odontólogos Asociados</h1>
 
-      <div className="hidden md:flex items-center gap-4">
-        {user && (
-          <span className="text-sm">
-            {user.role === "paciente" ? "👤 Paciente:" : "🩺 Médico:"}{" "}
-            <strong>{user.email}</strong>
-          </span>
-        )}
-      </div>
-    </header>
-  );
+			<div className='hidden md:flex items-center gap-4'>
+				{user && (
+					<span className='text-md'>
+						{user.role === "paciente" ? "👤 Paciente:" : "🩺 Médico:"}{" "}
+						<strong>{user.email}</strong>
+					</span>
+				)}
+			</div>
+		</header>
+	);
 };
 
 export default Topbar;
